5ac84f804cd6b974921f85e123a9abe2da75c408
Xiaochen Yin <xiaochenyin@Xiaochens-MacBook-Pro-2020.local>
2020-06-08 15:30:21 -0700

    add comments

--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -1,4 +1,7 @@
-makeCacheMatrix <- function(x = matrix(,...)) {
+# This function uses the <<- operator which can be used to assign a value to an object in an environment that is different from the current environment. 
+# This function is used to create a special object that stores a matrix and cache's its inverse.
+
+makeCacheMatrix <- function(x = matrix()) {
     m <- NULL
     set <- function(y) {
         x <<- y
